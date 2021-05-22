157be6f9f4158b90bd03d2e6c976715839bc2bdd
RyanKhanna <81882189+RyanKhanna@users.noreply.github.com>
2021-05-22 18:35:39 -0500

    Add files via upload

---
+++ ADV 77/main.js
@@ -0,0 +1,9 @@
+//varibles
+var first_name = "Ryan";
+var last_name = " Khanna";
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+var a = 100;
+var b = 50;
+var sum = a + b;
+console.log(sum);
